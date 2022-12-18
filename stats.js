05f87ac361ad6f081187dfb00bad3a799b7c62ac
John de St. Germain <jdestgermain@users.noreply.github.com>
2022-12-17 18:12:15 -0600

    Renaming variables to make it more clear

--- stats.js
+++ stats.js
@@ -7,38 +7,42 @@ document.getElementById("calculateStats").addEventListener("click", function () 
         var csvString = reader.result;
         var rows = csvString.split("\n");
 
-        // Initialize an object to store the counts of values with different lengths
+        // Initialize an object to store the counts of chordMaps with different lengths
         var lengthCounts = {};
-        var valueCounts = {};
-        var wordCounts = {};
-        // Calculate the counts of values with different lengths
+        var chordMapCounts = {};
+        var lettersCounts = {};
+        var dupWords = [];
+        var wordGroups = {};
+        
+        // Calculate the counts of chordMaps with different lengths
         rows.forEach(function (row) {
             var cells = row.split(",");
-            var value = cells[0];
-            value = value.replace(/[\s+]+/g, ' ');
-            var valueLength = value.split(' ').length;
-            if (!lengthCounts[valueLength]) {
-                lengthCounts[valueLength] = 1;
+            var chordMap = cells[0];
+            chordMap = chordMap.replace(/[\s+]+/g, ' ');// Get rid of extra spaces and +
+            var chordMapLength = chordMap.split(' ').length;
+            if (!lengthCounts[chordMapLength]) {
+                lengthCounts[chordMapLength] = 1;
             }
             else {
-                lengthCounts[valueLength]++;
+                lengthCounts[chordMapLength]++;
             }
-            // Increment the count for the value in the second column
-            var secondColumnValue = cells[1];
-            if (!valueCounts[secondColumnValue]) {
-                valueCounts[secondColumnValue] = 1;
+            // Increment the count for the chordMap in the second column
+            var chord = cells[1];
+            if (!chordMapCounts[chord]) {
+                chordMapCounts[chord] = 1;
             }
             else {
-                valueCounts[secondColumnValue]++;
+                chordMapCounts[chord]++;
+                dupWords.push(chord)
             }
 
-            var words = value.split(' ');
-            words.forEach(function (word) {
-                if (!wordCounts[word]) {
-                    wordCounts[word] = 1;
+            var letters = chordMap.split(' ');
+            letters.forEach(function (letter) {
+                if (!lettersCounts[letter]) {
+                    lettersCounts[letter] = 1;
                 }
                 else {
-                    wordCounts[word]++;
+                    lettersCounts[letter]++;
                 }
             })
         });
@@ -50,15 +54,15 @@ document.getElementById("calculateStats").addEventListener("click", function () 
         summaryTable += "</table>";
 
         // Calculate frequencies of letters/keys used
-        var sortedWordCounts = Object.entries(wordCounts).sort((a, b) => b[1] - a[1]);
+        var sortedLettersCounts = Object.entries(lettersCounts).sort((a, b) => b[1] - a[1]);
         var wordFrequencyTable = "<table><tr><th>Letter/Key</th><th>Count</th></tr>";
         
         // Use an object to store words with the same count
         var countToWords = {};
         
-        for (var i = 0; i < sortedWordCounts.length; i++) {
-            var currentCount = sortedWordCounts[i][1];
-            var currentWord = sortedWordCounts[i][0];
+        for (var i = 0; i < sortedLettersCounts.length; i++) {
+            var currentCount = sortedLettersCounts[i][1];
+            var currentWord = sortedLettersCounts[i][0];
         
             // If this count is not in the object yet, add it
             if (!(currentCount in countToWords)) {
@@ -86,8 +90,9 @@ document.getElementById("calculateStats").addEventListener("click", function () 
         wordFrequencyTable += "</table>";
         
         
-        var uniqueValuesCount = Object.keys(valueCounts).length;
-        document.getElementById("stats").innerHTML = "Number of chords: " + rows.length + "<br>Number of unique words chorded: " + uniqueValuesCount + "<br>" + summaryTable + "<br>" + wordFrequencyTable;
+        var uniquechordMapsCount = Object.keys(chordMapCounts).length;
+        document.getElementById("stats").innerHTML = "Number of chords: " + rows.length + "<br>Number of unique words chorded: "  + uniquechordMapsCount + 
+        "<br>Duplicate words: "+dupWords.join(', ') + "<br><br>" + summaryTable + "<br>" + wordFrequencyTable;
     };
 
     reader.readAsText(inputFile);
