a1733ad051a5e5898d36dcb431ebfc33672b47ca
John de St. Germain <jdestgermain@users.noreply.github.com>
2023-08-14 22:21:26 -0500

    Closes 17

---
+++ js/diagnostic.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('input');
+let events = [];
+input.addEventListener('keyup', (event) => {
+    events.push({ event, time: performance.now() });
+});
+input.addEventListener('keydown', (event) => {
+    events.push({ event, time: performance.now() });
+});
+const plotButton = document.querySelector('#plot');
+const resetButton = document.querySelector('#reset');
+plotButton.addEventListener('click', () => {
+    const container = document.getElementById('visualization');
+    container.replaceChildren();
+    const items = new vis.DataSet(events.map(({ event, time }) => {
+        const color = event.type === 'keyup' ? 'red' : 'green';
+        return {
+            content: event.code,
+            start: time,
+            style: `color: white`,
+            className: color
+        };
+    }));
+    const options = {};
+    const timeline = new vis.Timeline(container, items, options);
+});
+resetButton.addEventListener('click', () => {
+    const container = document.getElementById('visualization');
+    container.replaceChildren();
+    events = [];
+    input.value = '';
+});
