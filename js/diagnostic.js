a9155a54a35d3e078df7756df60656ca05db8f00
John de St. Germain <jdestgermain@users.noreply.github.com>
2023-08-24 09:03:28 -0500

    Make the visualization relative time

--- js/diagnostic.js
+++ js/diagnostic.js
@@ -17,7 +17,7 @@ plotButton.addEventListener('click', () => {
         const color = event.type === 'keyup' ? 'red' : 'green';
         return {
             content: event.code,
-            start: time,
+            start: time - events[0].time,
             style: `color: white`,
             className: color
         };
@@ -26,7 +26,10 @@ plotButton.addEventListener('click', () => {
         orientation: 'top',
         align: 'left',
         order: (a, b) => b.time - a.time,
-        timeAxis: { scale: 'millisecond', step: 10 }
+        showMajorLabels: false,
+        format: {
+            minorLabels: (date) => date.valueOf().toString(),
+        }
     };
     const timeline = new vis.Timeline(container, items, options);
 
