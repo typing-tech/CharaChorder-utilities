c5230e17244ac1368e5a2df5919329550f7c9338
John de St Germain <jdestgermain@users.noreply.github.com>
2023-08-29 17:55:15 -0500

    Add word lists

--- src/components/CC1ChordGenerator.js
+++ src/components/CC1ChordGenerator.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Table, TableBody, TableCell, TableHead, TableRow, TextField, Button, Slider, Typography, FormControlLabel, Switch, Divider, Box } from '@mui/material';
+import {
+    Table, TableBody, TableCell, TableHead, TableRow, TextField, Button,
+    Select, MenuItem, Slider, Typography, FormControlLabel, Switch, Divider, Box,
+    InputAdornment
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import ClearIcon from '@mui/icons-material/Clear';
 import { generateChords } from '../functions/chordGeneration'
 import Papa from 'papaparse';
+import english500 from '../words/english-500.json';
+import english1000 from '../words/english-1000.json';
+import english5000 from '../words/english-5000.json';
 
 function CC1ChordGenerator({ chordLibrary }) {
     const [createdChords, setCreatedChords] = useState({});
@@ -10,7 +19,7 @@ function CC1ChordGenerator({ chordLibrary }) {
     const [checkboxStates, setCheckboxStates] = useState({
         useDupKey: true,
         useMirroredKeys: true,
-        useAltKeys: true,
+        useAltKeys: false,
         use3dKeys: false,
         createCsv: false,
     });
@@ -32,9 +41,11 @@ function CC1ChordGenerator({ chordLibrary }) {
     };
     const fileInputRef = useRef(null);
 
+    // Word set dropdown state
+    const [selectedWordSet, setSelectedWordSet] = useState(null);
 
     const clearCsv = () => {
-        setNumRowsToUse(0); 
+        setNumRowsToUse(0);
         setCsvWords([]);
         setIsFileUploaded(false);
         if (fileInputRef.current) {
@@ -44,6 +55,10 @@ function CC1ChordGenerator({ chordLibrary }) {
 
     const handleChange = (e) => {
         setInputWords(e.target.value);
+    };
+
+    const clearInput = () => {
+        setInputWords('');
     };
 
     const handleSliderChange = (event, newValue) => {
@@ -57,22 +72,35 @@ function CC1ChordGenerator({ chordLibrary }) {
         });
     };
 
-    useEffect(() => {
-        setNumRowsToUse(Math.min(200, csvWords.length));
-    }, [csvWords]);
-
     const handleClick = async () => {
         const wordsArray = csvWords.length > 0 ? csvWords.slice(0, numRowsToUse) : inputWords.split(',').map(word => word.trim());
         const generatedChords = await generateChords(wordsArray, sliderValue, checkboxStates, chordLibrary);
         setCreatedChords(generatedChords);
     };
 
+    const wordSets = {
+        'english500': english500.words,
+        'english1000': english1000.words,
+        'english5000': english5000.words,
+    };
+
+    useEffect(() => {
+        setNumRowsToUse(Math.min(200, csvWords.length));
+        if (wordSets[selectedWordSet]) {
+            setInputWords(wordSets[selectedWordSet].join(', '));
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [csvWords, selectedWordSet ]);
+
     const createdChordsArray = Object.entries(createdChords);
 
     return (
         <div>
             <Typography style={{ margin: '10px 0' }}>
-                Either upload a .csv file that has words sorted by importance in the first column (such as a CharaChorder Nexus export) or type words separated by commas.
+                Either upload a .csv file that has words sorted by importance in the first column (such as a CharaChorder Nexus export),
+                type words separated by commas, or choose from a predetermined set of word lists. Note: using the word lists can take
+                quite a bit of time, so you may have to wait a few minutes.
             </Typography>
             <Box display="flex" flexDirection="row" alignItems="center" gap="20px">
                 <Button variant="contained" component="label">
@@ -115,9 +143,31 @@ function CC1ChordGenerator({ chordLibrary }) {
                         value={inputWords}
                         onChange={handleChange}
                         fullWidth
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={clearInput}>
+                                        <ClearIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
                     />
                 </>
             )}
+
+            <Select
+                value={selectedWordSet}
+                onChange={(e) => setSelectedWordSet(e.target.value)}
+                displayEmpty
+            >
+                <MenuItem value="" disabled>
+                    Select a word set
+                </MenuItem>
+                <MenuItem value="english500">Top 500 English Words</MenuItem>
+                <MenuItem value="english1000">Top 1000 English Words</MenuItem>
+                <MenuItem value="english5000">Top 5000 English Words</MenuItem>
+            </Select>
 
             <Divider style={{ margin: '20px 0' }}></Divider>
             <Typography gutterBottom>Select number of keys for the chord inputs</Typography>
